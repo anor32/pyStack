ab18712288299bf12144c4f33fb4d73a47c8bb8e
anor23 <f@>
2024-12-19 12:35:40 +0300

    документация к методам написана

--- Stack.py
+++ Stack.py
@@ -1,13 +1,19 @@
 class Node:
+    """основной класс для связывания узлами принимает текущий узел и следующий"""
+
     def __init__(self, data, next_node=None):
         self.data = data
         self.next_node = next_node
 
 
 class Stack:
+    """класс  Стек для хранения элементов внутри инциализируется размером и верхним элементом"""
+
     def __init__(self, stack_size=5, top=None):
         self.stack_size = stack_size
         self.top = top  # через топ обращаемся к атрибутам ноды
+
+    """добавления элемента в стек принимает элемент добавляет в стек"""
 
     def push(self, data):
         if self.size_stack() < self.stack_size:
@@ -18,6 +24,8 @@ class Stack:
             print("Стэк переполнен")
             return "Стэк переполнен"
 
+    """удаление элемента из стека принимает элемент удаляет его"""
+
     def pop(self):
         if self.top:
             remove_last = self.top
@@ -26,11 +34,15 @@ class Stack:
         else:
             return "Стэк пуст"
 
+    """проврека на то пуст ли стек возвращает true или false"""
+
     def is_empty(self):
         if self.top:
             return False
         else:
             return True
+
+    """проврека на то полон ли стек возвращает true или false"""
 
     def is_full(self):
         if self.stack_size == self.size_stack():
@@ -38,9 +50,13 @@ class Stack:
         else:
             return False
 
+    """очистка стека пока полный удаляет элементы """
+
     def clear_stack(self):
         while self.top:
             self.pop()
+
+    """получение элемента по индесу принимает индекс возвращает элемент"""
 
     def get_data(self, index):
         counter = 0
@@ -52,6 +68,8 @@ class Stack:
             counter += 1
         return f"Out of range"
 
+    """получение длины стека возвращает длину стека"""
+
     def size_stack(self):
         counter = 0
         stack_item = self.top
@@ -59,7 +77,7 @@ class Stack:
             counter += 1
             stack_item = stack_item.next_node
         return counter
-
+    """подсчет целых чисел возвращает количество  целых чисел """
     def counter_int(self):
         counter = 0
         stack_item = self.top
@@ -70,10 +88,14 @@ class Stack:
         return counter
 
 
+#постарался по принципу 3 вопросов (что делает что принмает что возвращает) писать докстринги который вы сказали в конце урока
+# я могу конечно детальнее расписать что делает каждый метод но как я понял лучше кратко писать
+
 stack = Stack()
 stack.push(1)
 stack.push("sta")
 stack.push(2)
 stack.push(2.5)
 stack.push("sta")
+
 print(stack.counter_int())
